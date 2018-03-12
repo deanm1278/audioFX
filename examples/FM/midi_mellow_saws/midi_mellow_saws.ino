e4f0ff15c74bf72858a502038e99f52de4a63520
dean <deanm1278@gmail.com>
2018-03-12 19:59:38 -0400

    fix tuning problems

--- examples/FM/midi_mellow_saws/midi_mellow_saws.ino
+++ examples/FM/midi_mellow_saws/midi_mellow_saws.ino
@@ -1,11 +1,11 @@
-/* This is a sample epiano patch with vibrato effect.
- * 8 voices, 4 operators
+/* 4 operators w/ feedback, 8 voice poly
  */
 
 #include "MIDI.h"
 #include "audioFX.h"
 #include "fm.h"
 #include "midi_notes.h"
+
 
 MIDI_CREATE_INSTANCE(HardwareSerial, Serial1,  MIDI);
 
@@ -14,6 +14,14 @@ q31 *dataPtr;
 
 static RAMB q31 outputData[AUDIO_BUFSIZE];
 static uint32_t roundRobin = 0;
+
+LFO<q16> lfo( _F16(6.0) );
+#define LFO_DEPTH 0.01
+
+#define ATTACK_LVL .24
+#define SUSTAIN_LVL .2
+
+#define DECAY_TIME 100
 
 void handleNoteOn(byte channel, byte pitch, byte velocity);
 void handleNoteOff(byte channel, byte pitch, byte velocity);
@@ -30,81 +38,79 @@ Algorithm A(ops, NUM_OPERATORS);
 Voice v1(&A), v2(&A), v3(&A), v4(&A), v5(&A), v6(&A), v7(&A), v8(&A);
 Voice *voices[NUM_VOICES] = {&v1, &v2, &v3, &v4, &v5, &v6, &v7, &v8};
 
-#define LFO_DEPTH .25
-#define LFO_RATE 5.0
-LFO<q31> lfo( _F16(LFO_RATE) );
-
-RatioFrequency op2Ratio(&op1, _F16(4.0));
-RatioFrequency op4Ratio(&op1, _F16(1.0));
+RatioFrequency op2Ratio(&op1, _F16(2.005));
+RatioFrequency op3Ratio(&op1, _F16(2.006));
+RatioFrequency op4Ratio(&op1, _F16(2.0));
 
 void setup()
 {
    /******* DEFINE STRUCTURE *******
-   *                    /\
-   *           OP2    OP4/
-   *            |      |
-   *           OP1    OP3
-   *             \    /
-   *             output
+   *           /\   /\     /\   /\
+   *           \OP1 \OP2 OP3/ OP4/
+   *               \  \  /   /
+   *                  output
    */
   op1.isOutput = true;
+  op2.isOutput = true;
   op3.isOutput = true;
-
-  op1.mods[0] = &op2;
-  op3.mods[0] = &op4;
+  op4.isOutput = true;
 
   //******** DEFINE MODULATOR FREQUENCIES ********//
   op2.setCarrier(&op2Ratio);
+  op3.setCarrier(&op3Ratio);
   op4.setCarrier(&op4Ratio);
-  op4.feedbackLevel = _F(.05);
+  op1.feedbackLevel = _F(.999);
+  op2.feedbackLevel = _F(.999);
+  op3.feedbackLevel = _F(.999);
+  op4.feedbackLevel = _F(.999);
 
   //******* DEFINE ENVELOPES *******//
 
   //***** OP1 ENVELOPE *****//
   op1.volume.attack.time = 8;
-  op1.volume.attack.level = _F(.5);
+  op1.volume.attack.level = _F(ATTACK_LVL);
 
-  op1.volume.decay.time = 200;
-  op1.volume.decay.level = _F(.4);
+  op1.volume.decay.time = DECAY_TIME;
+  op1.volume.decay.level = _F(SUSTAIN_LVL);
 
-  op1.volume.decay2.time = 2000;
+  op1.volume.sustain.level = _F(SUSTAIN_LVL);
 
-  op1.volume.sustain.level = _F(0);
+  op1.volume.release.time = 25;
 
   //***** OP2 ENVELOPE *****//
-  op2.volume.attack.time = 1;
-  op2.volume.attack.level = _F(.1);
+  op2.volume.attack.time = 8;
+  op2.volume.attack.level = _F(ATTACK_LVL);
 
-  op2.volume.decay.time = 50;
-  op2.volume.decay.level = _F(.05);
+  op2.volume.decay.time = DECAY_TIME;
+  op2.volume.decay.level = _F(SUSTAIN_LVL);
 
-  op2.volume.decay2.time = 1000;
+  op2.volume.sustain.level = _F(SUSTAIN_LVL);
 
-  op2.volume.sustain.level = _F(0);
+  op2.volume.release.time = 25;
 
   //***** OP3 ENVELOPE *****//
   op3.volume.attack.time = 8;
-  op3.volume.attack.level = _F(.45);
+  op3.volume.attack.level = _F(ATTACK_LVL);
 
-  op3.volume.decay.time = 100;
-  op3.volume.decay.level = _F(.3);
+  op3.volume.decay.time = DECAY_TIME;
+  op3.volume.decay.level = _F(SUSTAIN_LVL);
 
-  op3.volume.decay2.time = 2000;
+  op3.volume.sustain.level = _F(SUSTAIN_LVL);
 
-  op3.volume.sustain.level = _F(.0);
+  op3.volume.release.time = 25;
 
   //***** OP4 ENVELOPE *****//
-  op4.volume.attack.time = 1;
-  op4.volume.attack.level = _F(.1);
+  op4.volume.attack.time = 8;
+  op4.volume.attack.level = _F(ATTACK_LVL);
 
-  op4.volume.decay.time = 100;
-  op4.volume.decay.level = _F(.01);
+  op4.volume.decay.time = DECAY_TIME;
+  op4.volume.decay.level = _F(SUSTAIN_LVL);
 
-  op4.volume.decay2.time = 2000;
+  op4.volume.sustain.level = _F(SUSTAIN_LVL);
 
-  op4.volume.sustain.level = _F(0);
+  op4.volume.release.time = 25;
 
-  lfo.depth = _F(LFO_DEPTH);
+  lfo.depth = _F16(LFO_DEPTH);
 
   //***** END OF PATCH SETUP *****//
 
@@ -132,12 +138,13 @@ void loop()
     MIDI.read();
 
     if(bufferReady){
+
     memset(outputData, 0, AUDIO_BUFSIZE*sizeof(q31));
 
     for(int i=0; i<NUM_VOICES; i++)
-      voices[i]->play(outputData, _F(.99/(NUM_VOICES-4)));
+      voices[i]->play(outputData, _F(.99/(NUM_VOICES-4)), &lfo);
 
-    lfo.getOutput(outputData);
+    //lfoVol.getOutput(outputData);
 
     q31 *ptr = outputData;
     for(int i=0; i<AUDIO_BUFSIZE; i++){
@@ -179,7 +186,7 @@ void handleNoteOff(byte channel, byte pitch, byte velocity)
   for(int i=0; i<NUM_VOICES; i++){
     //look for the note that was played
     if(voices[i]->active && voices[i]->output == midi_notes[pitch]){
-      voices[i]->trigger(false);
+      voices[i]->trigger(false, true);
       break;
     }
   }

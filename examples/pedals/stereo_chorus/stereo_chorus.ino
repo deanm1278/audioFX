0735c3678cc0f6004a5c305784abb0f28a3441b6
dean <deanm1278@gmail.com>
2018-04-30 18:44:18 -0400

    stereo chorus pedal

--- examples/pedals/stereo_chorus/stereo_chorus.ino
+++ examples/pedals/stereo_chorus/stereo_chorus.ino
@@ -2,82 +2,57 @@
 #include "adau17x1.h"
 #include "Adafruit_ADS1015.h"
 #include "fm.h"
+#include "delay.h"
 
-#define ADC_SCALE_FACTOR 1048576
-#define RATE_SCALE_FACTOR 200
+#define ADC_SCALE_FACTOR 500000
 
 /* the maximum number of samples the chorus voice will be delayed */
 #define DELAY_MAX_ORDER (10)
-#define MAX_DELAY (1UL << DELAY_MAX_ORDER)
-#define DELAY_BUFSIZE ((MAX_DELAY/AUDIO_BUFSIZE) * AUDIO_BUFSIZE)
+#define MAX_DELAY ((1UL << DELAY_MAX_ORDER)+AUDIO_BUFSIZE)
 
-q31 dataL[DELAY_BUFSIZE], dataR[DELAY_BUFSIZE];
+q31 RAMB dataL[MAX_DELAY], dataR[MAX_DELAY];
+q31 deinterleavedL[AUDIO_BUFSIZE], deinterleavedR[AUDIO_BUFSIZE];
+q31 t1L[AUDIO_BUFSIZE], t1R[AUDIO_BUFSIZE];
+q31 t2L[AUDIO_BUFSIZE], t2R[AUDIO_BUFSIZE];
 
-typedef struct {
-    q31 *head;
-    q31 count;
-    uint32_t currentOffset;
-    q31 *dptr;
-    q31 *data;
-} liteBuf;
+struct delayLine lineL = { dataL, dataL, MAX_DELAY, 0 };
+struct delayTap tap1L = { &lineL, dataL, 0, _F16(.01), 1023, 0x7FFFFFFF, };
+struct delayTap tap2L = { &lineL, dataL, 0, _F16(.01), 512, 0x7FFFFFFF, };
 
-static liteBuf bufL = {dataL, 0, 0, dataL, dataL};
-static liteBuf bufR = {dataR, 0, 0, dataR, dataR};
+struct delayLine lineR = { dataR, dataR, MAX_DELAY, 0 };
+struct delayTap tap1R = { &lineR, dataR, 0, _F16(.003), 768, 0x7FFFFFFF, };
+struct delayTap tap2R = { &lineR, dataR, 0, _F16(.003), 512, 0x7FFFFFFF, };
 
-static RAMB q31 lfoDataL[AUDIO_BUFSIZE], lfoDataR[AUDIO_BUFSIZE];
-LFO<q31> lfoLeft( _F16(0.5) );
-LFO<q31> lfoRight( _F16(0.6) );
+static q31 depth;
 
 Adafruit_ADS1015 ads;
 adau17x1 iface;
 
-void processChorus(liteBuf *buf, q31 *lfoData, q31 *interleavedData){
-    for(int i=0; i<AUDIO_BUFSIZE; i++){
-        uint32_t newOffset = lfoData[i] >> 22;
-
-        while(buf->currentOffset != newOffset){
-            if(newOffset > buf->currentOffset){
-                //move further back until we reach the correct offset
-                if(buf->dptr == buf->data) buf->dptr = buf->data + DELAY_BUFSIZE - 1;
-                else buf->dptr--;
-                buf->currentOffset++;
-            }
-            else{
-                //move forward until we reach the correct offset
-                buf->dptr++;
-                if(buf->dptr == buf->data + DELAY_BUFSIZE) buf->dptr = buf->data;
-                buf->currentOffset--;
-            }
-        }
-
-        *buf->head = *interleavedData;
-        buf->head++;
-        if(buf->head == buf->data + DELAY_BUFSIZE) buf->head = buf->data; //wrap around
-
-        //use the data in dptr
-         *interleavedData = *interleavedData + *buf->dptr;
-         interleavedData += 2;
-
-         //increment dptr again since we've added to the buffer
-         buf->dptr++;
-         if(buf->dptr == buf->data + DELAY_BUFSIZE) buf->dptr = buf->data;
-    }
-}
-
 void audioLoop(q31 *data)
 {
-    memset(lfoDataL, _F(.999), AUDIO_BUFSIZE*sizeof(q31));
-    lfoLeft.getOutput(lfoDataL);
-    memset(lfoDataR, _F(.999), AUDIO_BUFSIZE*sizeof(q31));
-    lfoRight.getOutput(lfoDataR);
+    q31 *ptr = data;
+    for(int i=0; i<AUDIO_BUFSIZE; i++){
+        deinterleavedL[i] = *ptr++;
+        deinterleavedR[i] = *ptr++;
+    }
+    _delay_push(&lineL, deinterleavedL, AUDIO_BUFSIZE);
+    _delay_push(&lineR, deinterleavedR, AUDIO_BUFSIZE);
 
-    processChorus(&bufL, lfoDataL, data);
-    processChorus(&bufR, lfoDataR, data + 1);
+    _delay_modulate(&tap1L, t1L, AUDIO_BUFSIZE);
+    _delay_modulate(&tap1R, t1R, AUDIO_BUFSIZE);
+
+    _delay_modulate(&tap2L, t2L, AUDIO_BUFSIZE);
+    _delay_modulate(&tap2R, t2R, AUDIO_BUFSIZE);
+
+    ptr = data;
+    for(int i=0; i<AUDIO_BUFSIZE; i++){
+        *ptr++ = __builtin_bfin_mult_fr1x32x32(t1L[i], depth) + __builtin_bfin_mult_fr1x32x32(t2L[i], depth) + *ptr;
+        *ptr++ = __builtin_bfin_mult_fr1x32x32(t1R[i], depth) + __builtin_bfin_mult_fr1x32x32(t2R[i], depth) + *ptr;
+    }
+
 }
 
 void setup(){
-    lfoLeft.depth = _F(.25);
-    lfoRight.depth = _F(.25);
 
     ads.setGain(GAIN_TWO);
     ads.begin();
@@ -92,13 +67,14 @@ void loop(){
     int16_t adc0, adc1, adc2;
 
     adc0 = ads.readADC_SingleEnded(0);
-    lfoLeft.depth = adc0 * ADC_SCALE_FACTOR/1.5;
-    lfoRight.depth = lfoLeft.depth;
+    depth = adc0 * ADC_SCALE_FACTOR;
 
     adc1 = ads.readADC_SingleEnded(1);
-    lfoLeft.rate = adc1 * RATE_SCALE_FACTOR;
+    tap1L.roc = adc1>>1;
+    tap2L.roc = adc1;
 
     adc2 = ads.readADC_SingleEnded(2);
-    lfoRight.rate = adc2 * RATE_SCALE_FACTOR;
-}
+    tap1R.roc = adc2>>1;
+    tap2R.roc = adc2;
 
+}
